b7e68ca189995fea556c3b48c677a4b802b42a90
Jon Anderson <jon1992@gmail.com>
2016-10-08 19:11:48 -0500

    .attributes and .multiples extracted to common area

---
+++ lib/objectmancy/common_class_methods.rb
@@ -0,0 +1,66 @@
+require 'objectmancy/errors'
+
+module Objectmancy
+   # Class methods mixed into anything including one of the mixins for
+   # Objectmancy
+  module CommonClassMethods
+    # @private
+    # Basic setup of the class-level state.
+    def self.extended(base)
+      base.instance_variable_set(:@registered_attributes, {})
+      base.class.send(:attr_reader, :registered_attributes)
+      base.send(:private_class_method, :attribute, :multiples)
+    end
+
+    # Defines an attribute usable by Objectmancy to create your object.
+    # Only attributes defined with this method will be converted to attributes
+    # on the final object.
+    #
+    # @param name [#to_sym] Attribute name
+    # @param opts [Hash] Options to be applied
+    # @option opts [Symbol, Class] :type The type of object to create. Can be
+    #   either a Symbol of one of: :datetime; else, a Class
+    # @option opts [Symbol, String] :objectable Method to call on :type. Will
+    #   default to calling :new. Ignored if :type is one of the known types.
+    #   Requires :type option.
+    # @raise [AttributeAlreadyDefinedError] Attempt to define two attributes
+    #   of the same name
+    # @raise [ArgumentError] Pass in :objectable without :type
+    def attribute(name, **opts)
+      symbolized_name = name.to_sym
+
+      if registered_attributes.key? symbolized_name
+        raise AttributeAlreadyDefinedError, name
+      end
+
+      if opts[:objectable] && opts[:type].nil?
+        raise ArgumentError, ':objectable option reuqires :type option'
+      end
+
+      registered_attributes[symbolized_name] = AttributeOptions.new(opts)
+
+      attr_accessor symbolized_name
+    end
+
+    # Allows the definition of Arrays of items to be turns into objects. Bear
+    # in mind that Arrays of basic objects (Strings, numbers, anything else
+    # that doesn't need special initialization) are handled by .attribute.
+    #
+    # @param name [#to_sym] Attribute name
+    # @param opts [Hash] Options to be applied
+    # @option opts [Symbol, Class] :type The type of object to create. Can be
+    #   either a Symbol of one of: :datetime; else, a Class
+    # @option opts [Symbol, String] :objectable Method to call on :type. Will
+    #   default to calling :new. Ignored if :type is one of the known types.
+    # @raise [AttributeAlreadyDefinedError] Attempt to define two attributes
+    #   of the same name
+    # @raise [ArgumentError] Calling .multiples without :type option
+    def multiples(name, **opts)
+      if opts[:type].nil?
+        raise ArgumentError, 'Multiples require the :type option'
+      end
+
+      attribute(name, opts.merge(multiple: true))
+    end
+  end
+end
--- lib/objectmancy/objectable.rb
+++ lib/objectmancy/objectable.rb
@@ -1,8 +1,9 @@
-require 'objectmancy/errors'
+require 'pry'
+
+require 'objectmancy/common_class_methods'
 require 'objectmancy/types'
 require 'objectmancy/attribute_options'
 
-require 'pry'
 
 module Objectmancy
   # Mixin for allowing your objects to take a Hash and turn them into an object.
@@ -22,70 +23,7 @@ module Objectmancy
   module Objectable
     # @private
     def self.included(base)
-      base.extend(ClassMethods)
-    end
-
-    # Class methods mixed into anything including Objectable. These are where
-    # the real power of Objectmancy comes from.
-    module ClassMethods
-      # @private
-      # Basic setup of the class-level state.
-      def self.extended(base)
-        base.instance_variable_set(:@registered_attributes, {})
-        base.class.send(:attr_reader, :registered_attributes)
-        base.send(:private_class_method, :attribute, :multiples)
-      end
-
-      # Defines an attribute usable by Objectmancy to create your object.
-      # Only attributes defined with this method will be converted to attributes
-      # on the final object.
-      #
-      # @param name [#to_sym] Attribute name
-      # @param opts [Hash] Options to be applied
-      # @option opts [Symbol, Class] :type The type of object to create. Can be
-      #   either a Symbol of one of: :datetime; else, a Class
-      # @option opts [Symbol, String] :objectable Method to call on :type. Will
-      #   default to calling :new. Ignored if :type is one of the known types.
-      #   Requires :type option.
-      # @raise [AttributeAlreadyDefinedError] Attempt to define two attributes
-      #   of the same name
-      # @raise [ArgumentError] Pass in :objectable without :type
-      def attribute(name, **opts)
-        symbolized_name = name.to_sym
-
-        if registered_attributes.key? symbolized_name
-          raise AttributeAlreadyDefinedError, name
-        end
-
-        if opts[:objectable] && opts[:type].nil?
-          raise ArgumentError, ':objectable option reuqires :type option'
-        end
-
-        registered_attributes[symbolized_name] = AttributeOptions.new(opts)
-
-        attr_accessor symbolized_name
-      end
-
-      # Allows the definition of Arrays of items to be turns into objects. Bear
-      # in mind that Arrays of basic objects (Strings, numbers, anything else
-      # that doesn't need special initialization) are handled by .attribute.
-      #
-      # @param name [#to_sym] Attribute name
-      # @param opts [Hash] Options to be applied
-      # @option opts [Symbol, Class] :type The type of object to create. Can be
-      #   either a Symbol of one of: :datetime; else, a Class
-      # @option opts [Symbol, String] :objectable Method to call on :type. Will
-      #   default to calling :new. Ignored if :type is one of the known types.
-      # @raise [AttributeAlreadyDefinedError] Attempt to define two attributes
-      #   of the same name
-      # @raise [ArgumentError] Calling .multiples without :type option
-      def multiples(name, **opts)
-        if opts[:type].nil?
-          raise ArgumentError, 'Multiples require the :type option'
-        end
-
-        attribute(name, opts.merge(multiple: true))
-      end
+      base.extend(CommonClassMethods)
     end
 
     # Creates your object. You should use the after_initialize
